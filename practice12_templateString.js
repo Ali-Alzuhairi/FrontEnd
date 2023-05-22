913457b33088702ebc79888e27d3d31d0cf3b577
Ali Alzuhairi <hod.20088@gmail.com>
2023-05-22 15:38:00 -0400

    js practice

---
+++ practice12_templateString.js
@@ -0,0 +1,29 @@
+const firstName = "Jamila";
+const lastName = "k";
+const age = 18;
+
+// print out first name is firstName 
+
+console.log("firstName is " + firstName);
+
+// ` backtick symbol for template string 
+
+console.log(`firstName is ${firstName} `); 
+
+// // print out full name is firstName lastName and she is age years old 
+console.log( `full name is ${firstName} ${lastName} and she is ${age} years old`);
+
+const multiline = 
+`this is line 1 
+this is line 2 
+this is line 3`;
+
+// console.log(multiline);
+
+console.log(`uppercase version is :   ${multiline.toUpperCase() } `);
+
+
+const num1 = 10 ;
+const num2 = 20 ;
+
+console.log(`the result of adding ${num1} to ${num2} is ${num1+num2}`);
